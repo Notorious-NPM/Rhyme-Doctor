440e50bdb4d2e44edb5e1fbbf508e0e6c7defd63
James Yen <jameshyen@gmail.com>
2018-03-13 15:18:31 -0700

    Need new version of Webpack

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component { // eslint-disable-line
+  constructor(props) { // eslint-disable-line
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Yo.</div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx'; // eslint-disable-line
+
+ReactDOM.render(<App />, document.getElementById('app')); // eslint-disable-line
