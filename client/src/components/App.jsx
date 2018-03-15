cc92491858df5ce8a2e84703f892d0ff3802a13b
elaskowski <evalaskows@gmail.com>
2018-03-14 19:40:52 -0700

    Create Thesaurus component folder with form & list

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import store from '../redux/store';
 import Login from './top/Login';
 import Paragraph from './text/Paragraph';
 import Friend from '../components/buttons/Friend';
+import ThesaurusForm from './thesaurus/ThesaurusForm.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,7 +38,12 @@ class App extends React.Component {
         {this.navSwitch()}
         <h3>Lyrics</h3>
         <Paragraph text={this.state.text} />
+<<<<<<< HEAD
         <Friend />
+=======
+        <br /><br />
+        <ThesaurusForm />
+>>>>>>> Create Thesaurus component folder with form & list
       </div>
     );
   }
---
+++ client/src/components/thesaurus/ThesaurusForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class ThesaurusForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: '',
+      synonyms: [],
+    }
+  }
+
+  searchWord(e) {
+    e.preventDefault();
+    axios
+      .get(`/api/word/synonym?word=${this.state.word}`)
+      .then((res) => {
+        this.setState({
+          synonyms: res.data
+        })
+      })
+  }
+
+  onChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+    <form>
+      Find me a word:<br/>
+      <input type="text" name="word" onChange={e => this.onChange(e)}></input><br/>
+      <input type="submit" onClick={(e) => this.searchWord(e)}></input>
+    </form>
+  );
+  }
+}
+
+
+// Line.propTypes = {
+//   line: PropTypes.string.isRequired,
+//   x: PropTypes.number.isRequired,
+// };
+
+export default ThesaurusForm;
