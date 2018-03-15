d7ab0a8e7ba3002e676bb432eaf1a3622c8afc8e
James Yen <jameshyen@gmail.com>
2018-03-15 11:19:19 -0700

    Linting pass

--- client/src/components/thesaurus/ThesaurusForm.jsx
+++ client/src/components/thesaurus/ThesaurusForm.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 
-import ThesaurusList from './ThesaurusList.jsx';
+import ThesaurusList from './ThesaurusList';
 
 class ThesaurusForm extends React.Component {
   constructor(props) {
@@ -10,7 +9,14 @@ class ThesaurusForm extends React.Component {
     this.state = {
       word: '',
       synonyms: [],
-    }
+    };
+  }
+
+  onChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   searchWord(e) {
@@ -19,36 +25,24 @@ class ThesaurusForm extends React.Component {
       .get(`/api/word/synonym?word=${this.state.word}`)
       .then((res) => {
         this.setState({
-          synonyms: res.data
-        })
-      })
-  }
-
-  onChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
+          synonyms: res.data,
+        });
+      });
   }
 
   render() {
     return (
-    <div>
-      <form>
-        Find me a word:<br/>
-        <input type="text" name="word" onChange={e => this.onChange(e)}></input><br/>
-        <input type="submit" onClick={(e) => this.searchWord(e)}></input>
-        <br /><br />
-      </form>
-      <ThesaurusList synonyms={this.state.synonyms}/>
-    </div>
+      <div>
+        <form>
+          Find me a word:<br />
+          <input type="text" name="word" onChange={e => this.onChange(e)} /><br />
+          <input type="submit" onClick={e => this.searchWord(e)} />
+          <br /><br />
+        </form>
+        <ThesaurusList synonyms={this.state.synonyms} />
+      </div>
     );
   }
 }
-
-
-ThesaurusForm.propTypes = {
-  synonyms: PropTypes.array.isRequired,
-};
 
 export default ThesaurusForm;
