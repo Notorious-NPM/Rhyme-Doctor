23e0563231aaa8dc61a93d7fa495b494a8fab6be
James Yen <jameshyen@gmail.com>
2018-03-14 19:44:00 -0700

    One-page apps...

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import store from '../redux/store.js';
+import Login from './top/Login.jsx';
 import Paragraph from './text/Paragraph.jsx';
 
 class App extends React.Component {
@@ -8,14 +9,32 @@ class App extends React.Component {
     super(props);
     this.state = store.getState();
     store.subscribe(() => {
-      this.state = store.getState();
+      this.setState(store.getState());
+      console.log(this.state);
     });
+  }
+
+  navSwitch = () => {
+    console.log('hm');
+    if (this.state.session) {
+      return (
+        <div>
+          <h2>You is logged in.</h2>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Login />
+      </div>
+    );
   }
 
   render() {
     return (
       <div align="center">
-        <h5>Lyrics</h5>
+        {this.navSwitch()}
+        <h3>Lyrics</h3>
         <Paragraph text={this.state.text} />
       </div>
     );
