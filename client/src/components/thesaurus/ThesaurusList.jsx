9ee3553081a2535f21c76a44550d6c75ea64b065
elaskowski <evalaskows@gmail.com>
2018-03-15 10:50:40 -0700

    Add ThesaurusList component

--- client/src/components/thesaurus/ThesaurusList.jsx
+++ client/src/components/thesaurus/ThesaurusList.jsx
@@ -1,40 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 class ThesaurusList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      word: '',
-      synonyms: [],
-    }
-  }
-
-  searchWord(e) {
-    e.preventDefault();
-    axios
-      .get(`/api/word/synonym?word=${this.state.word}`)
-      .then((res) => {
-        this.setState({
-          synonyms: res.data
-        })
-      })
-  }
-
-  onChange(e) {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
   }
 
   render() {
     return (
-    <form>
-      Find me a word:<br/>
-      <input type="text" name="word" onChange={e => this.onChange(e)}></input><br/>
-      <input type="submit" onClick={(e) => this.searchWord(e)}></input>
-    </form>
-  );
+      <div>
+        <div>{this.props.synonyms.map(synonym => 
+          <div>{synonym}</div>
+        )}
+        </div>
+      </div>
+    );
   }
 }
+
+ThesaurusList.propTypes = {
+  synonym: PropTypes.string.isRequired,
+};
+
+export default ThesaurusList;
