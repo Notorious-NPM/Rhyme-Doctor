41496d54a5e8ee93c4545cbfdcc2c55139f384a5
Kin Chan <xkinchanx@gmail.com>
2018-03-16 00:57:32 -0700

    [update]: front end updates to send socket msgs

--- client/src/components/buttons/FriendChat.jsx
+++ client/src/components/buttons/FriendChat.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Chat from './Chat';
 import axios from 'axios';
 import io from 'socket.io-client/dist/socket.io';
 
@@ -8,6 +9,8 @@ class FriendChat extends Component {
     this.state = {
       userID: 1,
       friendsList: {},
+      socket: null,
+      selectedChat: null,
     };
   }
 
@@ -21,10 +24,10 @@ class FriendChat extends Component {
       .then(({ data }) => {
         const friendsList = {};
         data.forEach((friendship) => {
-          if (friendship.userID === this.state.userID) {
-            friendsList[friendship.friendID] = false;
-          } else {
-            friendsList[friendship.userID] = false;
+          if (friendship.userID !== this.state.userID) {
+            friendsList[friendship.userID] = [friendship.roomID, false];
+          } else if (friendship.friendID !== this.state.userID) {
+            friendsList[friendship.friendID] = [friendship.roomID, false];
           }
         });
         this.setState({ friendsList }) // eslint-disable-line
@@ -32,29 +35,56 @@ class FriendChat extends Component {
       .catch(err => console.log('Friend componentMount error: ', err));
   }
 
-  createSocketConnection() {
-    const roomID = (Math.floor(Math.random() * 100000)).toString();
-
-    console.log(roomID);
-    console.log(typeof roomID);
-
-    this.socket = io('http://localhost:3444', {
+  async createSocketConnection() {
+    console.log('invoked createSocketConnection');
+    // this connection is for everyone so we can see who is logged on to chat
+    this.socket = await io('http://localhost:3444', {
       query: {
-        roomId: roomID,
+        roomId: 'main',
         title: 'friend chat',
       },
     });
+
+    await this.socket.emit('client.enter', { userID: this.state.userID });
+
+    this.setState({ socket: this.socket });
+
+    this.socket.on('server.enter', ({ userID }) => {
+      const { friendsList } = this.state;
+      if (friendsList[userID]) {
+        friendsList[userID][1] = true;
+        this.setState({ friendsList });
+      }
+    });
+  }
+
+  changeSelectedChat(friendID, roomID) {
+    this.setState({ selectedChat: [friendID, roomID] });
   }
 
   showState() {
     console.log(this.state);
   }
 
+  // emitEvent() {
+  //   const { socket } = this.state;
+
+  //   socket.emit('client.enter');
+  // }
+
   render() {
+    const { friendsList, userID, selectedChat } = this.state;
+
     return (
       <div>
         <button type="button" onClick={() => this.createSocketConnection()}>Show Friends</button>
+        <br />
+        <button type="button" onClick={() => this.showState()}>Show State</button>
+        <br />
         {/* <Chat /> */}
+        {Object.keys(friendsList).map(friendID =>
+          <div onClick={() => this.changeSelectedChat(friendID, friendsList[friendID][0])}>{friendID}</div>)}
+        {selectedChat && <Chat userID={userID} friendID={selectedChat[0]} roomID={selectedChat[1]} />}
       </div>
 
     );
