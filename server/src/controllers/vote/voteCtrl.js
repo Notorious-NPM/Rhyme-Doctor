84242b1b2127f477522825aee10db80d6704e363
iiffd <sml.hng@gmail.com>
2018-03-16 15:48:00 -0700

    Create endpoint to upvote rap post

--- server/src/controllers/vote/voteCtrl.js
+++ server/src/controllers/vote/voteCtrl.js
@@ -1,7 +1,25 @@
-// need DB helpers
+import User from '../../database/models/user';
+import UserLike from '../../database/models/user_like';
+import RapPost from '../../database/models/rap_post';
+
 
 const upvoteCtrl = (req, res) => {
-  res.status(201).end('Woo voted!');
+  // Check to see if user already liked post
+  UserLike.findOrCreate({ where: { user_id: req.body.userId, rap_post_id: req.body.rapPostId } })
+    .spread((_, created) => {
+      if (!created) {
+        res.status(409).send(JSON.stringify('Already liked'));
+      } else {
+        // Update post likes by one
+        RapPost.findById(Number(req.body.rapPostId))
+          .then((post) => {
+            return post.increment('like_count', { by: 1 });
+          })
+          .then(() => {
+            res.status(201).send(JSON.stringify('Success!'));
+          });
+      }
+    });
 };
 
 const downvoteCtrl = () => {
