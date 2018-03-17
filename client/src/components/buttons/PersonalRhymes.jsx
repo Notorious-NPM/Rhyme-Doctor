70d5d4a35603e71cde1a464f73b6d2ecca7d99d1
Kin Chan <xkinchanx@gmail.com>
2018-03-17 16:30:33 -0700

    [add]: setup for personal rhymes component

--- client/src/components/buttons/PersonalRhymes.jsx
+++ client/src/components/buttons/PersonalRhymes.jsx
@@ -15,18 +15,33 @@ class PersonalRhymes extends Component {
   }
 
   saveRhyme() {
+    const { word1, word2 } = this.state;
+    const option = {
+      word1,
+      word2,
+    };
+
     axios
-      .post('/api/')
-  };
+      .post('/api/rhymes/rhyme', option)
+      .then(({ data }) => console.log('saveRhyme post data: ', data))
+      .catch(err => console.log('saveRhyme post error: ', err));
+  }
+
+  showState() {
+    console.log(this.state);
+  }
 
   render() {
     return (
       <div>
         <form onChange={e => this.handleChange(e)}>
+          I want
           <input name="word1" type="text" />
+          to rhyme with
           <input name="word2" type="text" />
           <button type="button" onClick={() => this.saveRhyme()}>Submit</button>
         </form>
+        <button type="button" onClick={() => this.showState()}>showState</button>
       </div>
     );
   }
