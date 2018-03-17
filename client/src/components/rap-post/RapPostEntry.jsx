cc90950a8f0ca465d82ffa973a3cc53a88e81cca
iiffd <sml.hng@gmail.com>
2018-03-16 18:06:22 -0700

    Refactor for linter

--- client/src/components/rap-post/RapPostEntry.jsx
+++ client/src/components/rap-post/RapPostEntry.jsx
@@ -10,18 +10,20 @@ class RapPostEntry extends React.Component {
     };
   }
 
-  getComments = async (rapPostId) => {
-    const comments = await axios.get(`http://localhost:3000/api/content/comments/${rapPostId}`);
+  getComments = async () => {
+    const comments = await axios.get(`http://localhost:3000/api/content/comments/${this.props.rapPost.id}`);
     this.setState({
       comments: comments.data,
       showComments: !this.state.showComments,
     });
   }
 
-  likeRapPost = async (rapPostId, getRapPosts) => {
-    const status = await axios.put('http://localhost:3000/api/vote/upvote', {rapPostId: rapPostId});
+  likeRapPost = async () => {
+    const status = await axios.put('http://localhost:3000/api/vote/upvote',
+      { rapPostId: this.props.rapPost.id },
+    );
     console.log(status.data);
-    getRapPosts();
+    this.props.getRapPosts();
   }
 
   render() {
@@ -29,9 +31,9 @@ class RapPostEntry extends React.Component {
       <div>
         <p>{this.props.rapPost.text}</p>
         <br />
-        <p>By {this.props.rapPost.user.name} | <button onClick={this.likeRapPost.bind(null, this.props.rapPost.id, this.props.getRapPosts)}>Like</button> Like Count: {this.props.rapPost.like_count}</p>
+        <p>By {this.props.rapPost.user.name} | <button onClick={() => this.likeRapPost()}>Like</button> Like Count: {this.props.rapPost.like_count}</p>
         <br />
-        <button onClick={this.getComments.bind(null, this.props.rapPost.id)}>Show Comments</button>
+        <button onClick={() => this.getComments()}>Show Comments</button>
         {this.state.showComments ? this.state.comments.map((comment, i) =>
           <li key={i}>{comment.name}: {comment.text}</li>) : null}
       </div>
