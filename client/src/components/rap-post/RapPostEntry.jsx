980b511789ba1686a6ac98eab2544ca5862c5c6e
iiffd <sml.hng@gmail.com>
2018-03-16 18:00:30 -0700

    Render comments on client

--- client/src/components/rap-post/RapPostEntry.jsx
+++ client/src/components/rap-post/RapPostEntry.jsx
@@ -4,6 +4,18 @@ import axios from 'axios';
 class RapPostEntry extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      comments: [],
+      showComments: false,
+    };
+  }
+
+  getComments = async (rapPostId) => {
+    const comments = await axios.get(`http://localhost:3000/api/content/comments/${rapPostId}`);
+    this.setState({
+      comments: comments.data,
+      showComments: !this.state.showComments,
+    });
   }
 
   likeRapPost = async (rapPostId, getRapPosts) => {
@@ -18,6 +30,10 @@ class RapPostEntry extends React.Component {
         <p>{this.props.rapPost.text}</p>
         <br />
         <p>By {this.props.rapPost.user.name} | <button onClick={this.likeRapPost.bind(null, this.props.rapPost.id, this.props.getRapPosts)}>Like</button> Like Count: {this.props.rapPost.like_count}</p>
+        <br />
+        <button onClick={this.getComments.bind(null, this.props.rapPost.id)}>Show Comments</button>
+        {this.state.showComments ? this.state.comments.map((comment, i) =>
+          <li key={i}>{comment.name}: {comment.text}</li>) : null}
       </div>
     );
   }
