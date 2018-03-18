e74d8c7586b4966de52b1ffaf720e0b29253c464
Kin Chan <xkinchanx@gmail.com>
2018-03-17 17:33:56 -0700

    [add]: working on functions to create rhymes

--- server/src/controllers/rhymes/rhymeCtrl.js
+++ server/src/controllers/rhymes/rhymeCtrl.js
@@ -1,14 +1,17 @@
-// import helpers here
+import createPersonalRhyme from '../../database/dbHelpers/rhymeHelpers';
 
 const getRhymes = (req, res) => {
   res.status(200).send('rhymes');
 };
 
 const makePersonalRhyme = (req, res) => {
-  const { word1, word2 } = req.body;
-  // req.user.id
-
-  res.status(201).send('rhyme');
+  if (req.user.id) {
+    const { word1, word2 } = req.body;
+    createPersonalRhyme(req.user.id, word1, word2);
+    res.status(201).send('saved');
+  } else {
+    res.status(404).send('FORBIDDEN');
+  }
 };
 
 export { getRhymes, makePersonalRhyme };
--- server/src/database/dbHelpers/rhymeHelpers.js
+++ server/src/database/dbHelpers/rhymeHelpers.js
@@ -1,3 +1,27 @@
 // accept mongoDB
+import { PersonalRhyme } from '../mongoDB/index';
 
+const createPersonalRhyme = (userID, word1, word2) => {
+  // check if user already is in mongo collection
+    // if yes, check if either word1 or word2 is in rhymes property
+      // if yes, check if the other word is in array
+        // if yes, dont do anything
+        // if no, add to array
+      // if no, add property and other word into array
+    // if no, add user to collection
 
+  const sickRhymes = new PersonalRhyme({
+    userID,
+    rhymes: {
+      word1: [word2],
+    },
+  });
+
+  sickRhymes.save(() => console.log('sickRhymes saved to PersonaRhymes table'));
+};
+
+const queryPersonalRhyme = (userID, word) => {
+  console.log('reached queryPersonalRhyme');
+};
+
+export { createPersonalRhyme, queryPersonalRhyme };
