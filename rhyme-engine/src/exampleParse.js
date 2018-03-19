34ee9add6b79123161be0f98425d6cbf496c97fa
James Yen <jameshyen@gmail.com>
2018-03-19 14:05:59 -0700

    Ok...

--- rhyme-engine/src/exampleParse.js
+++ rhyme-engine/src/exampleParse.js
@@ -2,13 +2,15 @@ import unirest from 'unirest';
 import substrings from 'common-substrings';
 import API_KEY from './config';
 
+const fillString = '#';
+
 const lyrics = `I have some jello
 it makes me mellow
 I am an HR fellow
 Finished shit on hello`;
 
 const API = word =>
-  new Promise((resolve, reject) => {
+  new Promise((resolve) => {
     unirest.get(`https://wordsapiv1.p.mashape.com/words/${word}/pronunciation`)
       .header('X-Mashape-Key', API_KEY)
       .header('X-Mashape-Host', 'wordsapiv1.p.mashape.com')
@@ -19,37 +21,35 @@ const API = word =>
 
 const parse = (text) => {
   const lines = text.split('\n');
-  // const orderedWords = {};
   const APIcalls = [];
   const words = lines.map((line) => {
     const wordsInLine = line.split(' ');
     return wordsInLine[wordsInLine.length - 1];
   });
-  words.forEach((word, index) => {
+  words.forEach((word) => {
     APIcalls.push(API(word));
-    /* API(word)
-      .then((response) => {
-        console.log(response.pronunciation);
-        console.log(index);
-        orderedWords[index] = JSON.parse(JSON.stringify(response.pronunciation));
-      }); */
   });
   Promise.all(APIcalls)
     .then((data) => {
-      const rip = data.map(({ pronunciation }) => // eslint-disable-line
-        typeof pronunciation === 'object' && 'all' in pronunciation ? pronunciation.all : pronunciation);
-      console.log(substrings.weigh(rip));
+      const rip = data.map(({ pronunciation }) => {
+        if (typeof pronunciation === 'object' && 'all' in pronunciation) {
+          return pronunciation.all;
+        } else if (pronunciation) {
+          return pronunciation;
+        }
+        return fillString; // ah... coupling
+      });
+      const rhymes = [];
+      for (let i = 0; i < rip.length - 1; i += 1) {
+        for (let k = i + 1; k < rip.length; k += 1) {
+          if (k - i > 2) {
+            break;
+          }
+          rhymes.push([[words[i], words[k]], JSON.stringify(substrings.weigh([rip[i], rip[k]]))]);
+        }
+      }
+      console.log(rhymes);
     });
-  /* console.log(orderedWords);
-  const keys = Object.keys(orderedWords);
-  keys.forEach(key => console.log(orderedWords[key])); */
 };
-parse(lyrics);
 
-/* unirest.get(`https://wordsapiv1.p.mashape.com/words/${'hat'}/pronunciation`)
-  .header('X-Mashape-Key', API_KEY)
-  .header('X-Mashape-Host', 'wordsapiv1.p.mashape.com')
-  .end((response) => {
-    console.log(response.body);
-    res.status(200).end(JSON.stringify(response.body));
-  }); */
+parse(lyrics);
