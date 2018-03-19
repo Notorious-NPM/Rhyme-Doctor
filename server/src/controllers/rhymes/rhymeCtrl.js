92c46bac14c7b1b66075ae697d1d4d4199055f97
Kin Chan <xkinchanx@gmail.com>
2018-03-19 16:30:52 -0700

    [delete]: scrapping mongoDB in favor of mysql relational db

--- server/src/controllers/rhymes/rhymeCtrl.js
+++ server/src/controllers/rhymes/rhymeCtrl.js
@@ -1,17 +1,11 @@
-import createPersonalRhyme from '../../database/dbHelpers/rhymeHelpers';
+// import db helpers
 
 const getRhymes = (req, res) => {
-  res.status(200).send('rhymes');
+  res.status(200).send('hi');
 };
 
 const makePersonalRhyme = (req, res) => {
-  if (req.user.id) {
-    const { word1, word2 } = req.body;
-    createPersonalRhyme(req.user.id, word1, word2);
-    res.status(201).send('saved');
-  } else {
-    res.status(404).send('FORBIDDEN');
-  }
+  res.status(200).send('hi');
 };
 
 export { getRhymes, makePersonalRhyme };
