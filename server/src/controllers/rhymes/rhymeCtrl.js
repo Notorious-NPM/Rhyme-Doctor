40a512d672ac3032bdce72380333d920e5dd3faa
Kin Chan <xkinchanx@gmail.com>
2018-03-19 18:54:48 -0700

    [add]: functions to make and search personal rhymes

--- server/src/controllers/rhymes/rhymeCtrl.js
+++ server/src/controllers/rhymes/rhymeCtrl.js
@@ -1,11 +1,29 @@
-// import db helpers
+import { createPersonalRhymes, searchPersonalRhymes } from '../../database/dbHelpers/rhymeHelpers'
 
 const getRhymes = (req, res) => {
-  res.status(200).send('hi');
+  if (req.user.id) {
+    let { word1, word2 } = req.body;
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+    searchPersonalRhymes(req.user.id, word1, word2)
+      .then(result => res.status(200).send(result))
+      .catch(err => console.log('getRhymes error: ', err));
+  } else {
+    res.status(404).send('Forbidden');
+  }
 };
 
 const makePersonalRhyme = (req, res) => {
-  res.status(200).send('hi');
+  let { word1, word2 } = req.body;
+  if (req.user.id) {
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+    createPersonalRhymes(req.user.id, word1, word2)
+      .then(() => res.status(201).send('personal rhyme saved!'))
+      .catch(err => console.log('makePersonalRhyme error: ', err));
+  } else {
+    res.status(404).send('Forbidden');
+  }
 };
 
 export { getRhymes, makePersonalRhyme };
--- server/src/database/dbHelpers/rhymeHelpers.js
+++ server/src/database/dbHelpers/rhymeHelpers.js
@@ -1,1 +1,21 @@
-// changing to msql
+import Sequelize from 'sequelize';
+import PersonalRhymes from '../models/personalRhymes';
+
+const { Op } = Sequelize;
+
+const createPersonalRhymes = (userID, word1, word2) =>
+  PersonalRhymes.create({
+    userID,
+    word1,
+    word2,
+  });
+
+const searchPersonalRhymes = (userID, word1, word2) =>
+  PersonalRhymes.findOne({
+    where: {
+      userID,
+      [Op.or]: [{ word1, word2 }, { word1: word2, word2: word1 }],
+    },
+  });
+
+export { createPersonalRhymes, searchPersonalRhymes };
