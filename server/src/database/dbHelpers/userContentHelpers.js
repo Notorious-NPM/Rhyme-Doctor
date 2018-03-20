0a92bb1f93f9d939a1f5feb99e42ea9f26e044d8
elaskowski <evalaskows@gmail.com>
2018-03-19 17:18:36 -0700

    Updated getUserDataHelper to omit certain data

--- server/src/database/dbHelpers/userContentHelpers.js
+++ server/src/database/dbHelpers/userContentHelpers.js
@@ -7,17 +7,13 @@ const getUserDataHelper = ({ username }) => User.findOne({
   where: {
     name: username,
   },
-  // attributes: { exclude: ['password', 'id'] },
-  include: [{
-    model: rapPost,
-  }],
+  attributes: { exclude: ['password', 'id'] },
 });
 
 const getUserPostsHelper = ({ username }) => rapPost.findAll({
   where: {
     username,
   },
-  // attributes: { exclude: ['id'] },
 });
 
 export { getUserDataHelper, getUserPostsHelper };
