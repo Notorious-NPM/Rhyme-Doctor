0a33eaa1696af565ecd274dfde31bd9b2a980e45
elaskowski <evalaskows@gmail.com>
2018-03-20 19:26:54 -0700

    bio stuff

--- client/src/components/user/Bio.jsx
+++ client/src/components/user/Bio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import store from '../../redux/store';
 
 
@@ -12,8 +13,7 @@ class Bio extends React.Component {
     });
   }
 
-  componentWillMount = () => {
-    console.log(this.state);
+  componentDidMount = () => {
     this.setState({
       bio: this.props.bio,
       showEdit: false,
@@ -37,13 +37,13 @@ class Bio extends React.Component {
     });
   }
 
-  // editBio = (e) => {
-  //   e.preventDefault();
-  //   this.setState({
-  //     showEdit: !this.state.showEdit,
-  //   });
-  //   console.log('state', this.state);
-  // }
+  editBio = (e) => {
+    // e.preventDefault();
+    // this.setState({
+    //   showEdit: !this.state.showEdit,
+    // });
+    console.log('state', this.state);
+  }
 
   render() {
     return (
@@ -62,10 +62,16 @@ class Bio extends React.Component {
             <button type="button" onClick={() => this.addBio()}>Submit</button>
           </div>)}
         {this.state.bio && (<div>{this.state.bio}</div>)}
-        {this.state.user === this.props.username ? <a>Edit Bio</a> : null}
+        {this.state.user === this.props.username ? <button>Edit Bio</button> : null}
       </div>
     );
   }
 }
+
+Bio.propTypes = {
+  bio: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
 
 export default Bio;
