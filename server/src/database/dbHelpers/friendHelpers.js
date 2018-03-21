118e1f9df481a06577a45700cc8e0047dcaf1da0
Kin Chan <xkinchanx@gmail.com>
2018-03-20 23:50:49 -0700

    [delete]: no longer need friend seed data now that we can make friends

--- server/src/database/dbHelpers/friendHelpers.js
+++ server/src/database/dbHelpers/friendHelpers.js
@@ -34,73 +34,7 @@ const checkIfFriends = (userID, username) =>
     }],
   });
 
-const queryFriendHelper = userID => 
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 7,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 7,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 1,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 1,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 2,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 2,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 3,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 3,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 4,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 4,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
-  // Friends.create({
-  //   userID: 8,
-  //   friendID: 5,
-  //   roomID: 'two',
-  // });
-  // Friends.create({
-  //   userID: 5,
-  //   friendID: 8,
-  //   roomID: 'two',
-  // });
-
+const queryFriendHelper = userID =>
   User.findOne({
     where: {
       id: userID,
