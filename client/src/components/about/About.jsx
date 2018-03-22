d964149b1c0c1309b42f77769ba3f76123962638
Kin Chan <xkinchanx@gmail.com>
2018-03-22 12:13:42 -0700

    [add]: additional content in About page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
 
+import './About.css';
+
 const About = () => (
-  <div>
-    About Page
+  <div className="aboutTop">
+    <div className="whitespace" />
+    <div className="goal">
+      Thank you for visiting our application!  We at Notorious NPM developed Rhyme Doctor
+      as a tool to help people, amateur and professional alike, to create sick rhymes and
+      be able to share it with the world.  Simply type or paste your lyrics, click {'"Hit API"'},
+      and see the wonderful rhyme structure display before you.
+    </div>
+    <div className="whitespace" />
+    <div className="challenges">
+      Technical challenges/achievements
+      <br />
+      We are proud to say that nothing like this has been realized before, and especially not to the
+      extent where people can visualize the rhyme schemes of their lyrics.  Our experienced staff
+      here at Notorious NPM spent countless hours to deliver the elaborate architecture and
+      algorithms of Rhyme Doctor that delivers rhyme visualization at breakneck speeds.  We hope you
+      enjoy it!
+    </div>
+    <div className="whitespace" />
+    Meet the staff of Notorious NPM
+    <div className="contributors top">
+      <div className="contributors container">
+        <div className="contributor">
+          <img src="https://thumbs.dreamstime.com/t/super-cool-potato-character-cartoon-style-vector-illustration-95541644.jpg" alt="potato"/>
+          <br />
+          Senior Software Engineer
+          <br />
+          Eva Laskowski
+        </div>
+        <div className="contributor">
+          <img src="https://thumbs.dreamstime.com/t/super-cool-potato-character-cartoon-style-vector-illustration-95541644.jpg" alt="potato"/>
+          <br />
+          Senior Software Engineer
+          <br />
+          James Yen
+        </div>
+        <div className="contributor">
+          <img src="https://thumbs.dreamstime.com/t/super-cool-potato-character-cartoon-style-vector-illustration-95541644.jpg" alt="potato"/>
+          <br />
+          Senior Software Engineer
+          <br />
+          Kin Chan
+        </div>
+        <div className="contributor">
+          <img src="https://thumbs.dreamstime.com/t/super-cool-potato-character-cartoon-style-vector-illustration-95541644.jpg" alt="potato"/>
+          <br />
+          Senior Software Engineer
+          <br />
+          Samuel Hong
+        </div>
+      </div>
+    </div>
+    <br />
+    <div className="tech">
+      Tech Stack
+    </div>
+    <div>
+      Made at Hack Reactor
+    </div>
   </div>
 );
 
