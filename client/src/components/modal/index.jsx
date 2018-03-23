96f10c202414ab2a0e463934556ee587dc89c931
iiffd <sml.hng@gmail.com>
2018-03-23 16:37:59 -0700

    Modal popup on click for rap post

--- client/src/components/modal/index.jsx
+++ client/src/components/modal/index.jsx
@@ -2,28 +2,23 @@ import React from 'react';
 import './modal.css';
 
 const Modal = (props) => {
-  return (
-    // The Modal
-    <div className="modal fade" id="myModal">
-      <div className="modal-dialog">
+  return (    
+    // <!-- Modal -->
+    <div className={props.hidden ? 'modal fade' : 'modal fade show'} id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden={props.hidden} style={props.hidden ? { display: 'none' } : { display: 'block' }}>
+      <div className="modal-dialog" role="document">
         <div className="modal-content">
-
-          {/* Modal Header */}
           <div className="modal-header">
-            <h4 className="modal-title">Modal Heading</h4>
-            <button type="button" className="close" data-dismiss="modal">&times;</button>
+            <h5 className="modal-title" id="exampleModalLongTitle">By {props.name}</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true" onClick={() => props.triggerModal()}>&times;</span>
+            </button>
           </div>
-
-          {/* <!-- Modal body --> */}
           <div className="modal-body">
-            Modal body..
+            {props.rapText}
           </div>
-
-          {/* <!-- Modal footer --> */}
           <div className="modal-footer">
-            <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => props.triggerModal()}>Close</button>
           </div>
-
         </div>
       </div>
     </div>
--- client/src/components/rap-post/RapPostEntry.jsx
+++ client/src/components/rap-post/RapPostEntry.jsx
@@ -18,6 +18,7 @@ class RapPostEntry extends React.Component {
       alertMessage: '',
       timer: undefined,
       likes: this.props.rapPost.like_count,
+      hidden: true,
     };
   }
 
@@ -100,9 +101,15 @@ class RapPostEntry extends React.Component {
     });
   }
 
+  triggerModal = () => {
+    this.setState({
+      hidden: !this.state.hidden,
+    });
+  }
+
   render() {
     const { username } = this.props.rapPost;
-
+    const rapText = this.props.rapPost.text.split('\n').map(line => <div className="rap-text">{line}</div>);
     return (
       <div className="col-md-6">
         <div className="card">
@@ -114,8 +121,8 @@ class RapPostEntry extends React.Component {
               By{' '}
               <Link to={{ pathname: '/profile', state: { username }}}>{username}</Link>
             </h5>
-            <div className="rapText">
-              <p className="card-text">{this.props.rapPost.text.split('\n').map(line => <div className="rap-text">{line}</div>)}</p>
+            <div className="rapText" onClick={() => this.triggerModal()}>
+              <p className="card-text">{rapText}</p>
             </div>
             <button className="btn btn-primary btn-space" onClick={() => this.getComments()}>Show Comments</button>
           </div>
@@ -127,7 +134,7 @@ class RapPostEntry extends React.Component {
             username={this.props.username}
           /> : null}
         </div>
-        <Modal />
+        <Modal name={username} rapText={rapText} hidden={this.state.hidden} triggerModal={this.triggerModal} />
       </div>
     );
   }
