2ae383b6268202ae32972e1a620b55679c4e9793
James Yen <jameshyen@gmail.com>
2018-03-23 18:37:43 -0700

    Finish

--- client/src/components/toolbar/ColorPicker.jsx
+++ client/src/components/toolbar/ColorPicker.jsx
@@ -23,56 +23,58 @@ class ColorPicker extends React.Component {
 
   render() {
     return (
-      <div>
-        <span
-          style={{
-            padding: '20px',
-            background: '#C62D42',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-        <span
-          style={{
-            padding: '20px',
-            background: '#E77200',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-        <span
-          style={{
-            padding: '20px',
-            background: '#4D8C57',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-        <span
-          style={{
-            padding: '20px',
-            background: '#2887C8',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-        <span
-          style={{
-            padding: '20px',
-            background: '#7070CC',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-        <span
-          style={{
-            padding: '20px',
-            background: '#8E3179',
-            margin: '5px',
-          }}
-          onClick={this.clickHandler}
-        />
-      </div>
+      <label htmlFor="colorpad">
+        <div id="colorpad">
+          <span
+            style={{
+              padding: '20px',
+              background: '#C62D42',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+          <span
+            style={{
+              padding: '20px',
+              background: '#E77200',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+          <span
+            style={{
+              padding: '20px',
+              background: '#4D8C57',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+          <span
+            style={{
+              padding: '20px',
+              background: '#2887C8',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+          <span
+            style={{
+              padding: '20px',
+              background: '#7070CC',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+          <span
+            style={{
+              padding: '20px',
+              background: '#8E3179',
+              margin: '5px',
+            }}
+            onClick={this.clickHandler}
+          />
+        </div>
+      </label>
     );
   }
 }
