2e6bdeb1647d4ec28996eb8c271e83e753ed6cb1
Kin Chan <xkinchanx@gmail.com>
2018-03-26 12:49:10 -0700

    [add]: limit results length to overflow

--- client/src/components/rhymeSearch/RhymeList.jsx
+++ client/src/components/rhymeSearch/RhymeList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const RhymeList = ({ rhymes }) => (
   <div>
-    <div>
+    <div style={{ overflow: 'auto', height: '300px' }}>
       {rhymes.map(rhyme => <div>{rhyme}</div>)}
     </div>
   </div>
