c43758bef1e804dcda3cabf7230b51727009aa26
Kin Chan <xkinchanx@gmail.com>
2018-03-26 13:14:35 -0700

    [add]: css updates for rhyme finder

--- client/src/components/rhymeSearch/RhymeList.jsx
+++ client/src/components/rhymeSearch/RhymeList.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 
 const RhymeList = ({ rhymes }) => (
   <div>
-    <div style={{ overflow: 'auto', height: '300px' }}>
+    <div style={{
+      overflow: 'auto',
+      height: '300px',
+      'background-color': '#3D3D3D',
+      border: '4px solid #262525',
+      }}
+    >
       {rhymes.map(rhyme => <div>{rhyme}</div>)}
     </div>
   </div>
