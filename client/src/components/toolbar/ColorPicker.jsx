356b011e0fe3e6bc51a5ce96bb41a7aac915db1e
iiffd <sml.hng@gmail.com>
2018-03-30 12:07:46 -0700

    Change color pad to shine on click

--- client/src/components/toolbar/ColorPicker.jsx
+++ client/src/components/toolbar/ColorPicker.jsx
@@ -46,82 +46,70 @@ class ColorPicker extends React.Component {
 
   render() {
     return (
-      <label className="colorpicker-padding" htmlFor="colorpad">
-        <div id="colorpad">
-          <span
-            style={{
-              padding: '20px',
-              background: '#C62D42',
-              margin: '5px',
-            }}
-            className={this.state.color1 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color1')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#E77200',
-              margin: '5px',
-            }}
-            className={this.state.color2 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color2')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#4D8C57',
-              margin: '5px',
-            }}
-            className={this.state.color3 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color3')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#2887C8',
-              margin: '5px',
-            }}
-            className={this.state.color4 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color4')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#7070CC',
-              margin: '5px',
-            }}
-            className={this.state.color5 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color5')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#8E3179',
-              margin: '5px',
-            }}
-            className={this.state.color6 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color6')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#93CCEA',
-              margin: '5px',
-            }}
-            className={this.state.color7 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color7')}
-          />
-          <span
-            style={{
-              padding: '20px',
-              background: '#FBE870',
-              margin: '5px',
-            }}
-            className={this.state.color8 ? 'color-tab active-tab' : 'color-tab'}
-            onClick={e => this.clickHandler(e, 'color8')}
-          />
-        </div>
-      </label>
+      <div>
+        <div className="solid-block-top">Color Pad:</div>
+        <label className="colorpicker-padding" htmlFor="colorpad">
+          <div id="colorpad">
+            <span
+              style={{
+                background: '#C62D42',
+              }}
+              className={this.state.color1 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color1')}
+            />
+            <span
+              style={{
+                background: '#E77200',
+              }}
+              className={this.state.color2 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color2')}
+            />
+            <span
+              style={{
+                background: '#4D8C57',
+              }}
+              className={this.state.color3 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color3')}
+            />
+            <span
+              style={{
+                background: '#2887C8',
+              }}
+              className={this.state.color4 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color4')}
+            />
+            <span
+              style={{
+                background: '#7070CC',
+              }}
+              className={this.state.color5 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color5')}
+            />
+            <span
+              style={{
+                background: '#8E3179',
+              }}
+              className={this.state.color6 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color6')}
+            />
+            <span
+              style={{
+                background: '#93CCEA',
+              }}
+              className={this.state.color7 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color7')}
+            />
+            <span
+              style={{
+                background: '#FBE870',
+              }}
+              className={this.state.color8 ? 'color-tab-effect' : 'color-tab'}
+              onClick={e => this.clickHandler(e, 'color8')}
+            />
+          </div>
+        </label>
+        <div className="solid-block-bottom" />
+      </div>
     );
   }
 }
