e4670dfea243d0d34242a339ef27db3c9aec5f11
James Yen <jameshyen@gmail.com>
2018-04-03 17:41:20 +0000

    Remove console logs

--- rhyme-engine/src/parse.js
+++ rhyme-engine/src/parse.js
@@ -72,11 +72,9 @@ const parse = (text, options) =>
     words.forEach((word) => {
       if (typeof word === 'string') {
         const normalized = word.replace(/[,.:;'"“”‘’()&?-]/g, ''); // Hyphen either at the beginning or end, or escaped, i.e. \-
-        // console.log(normalized);
         APIcalls.push(API(normalized));
       } else {
         APIcalls.push(new Promise((resolution) => {
-          // console.log(word);
           resolution(word);
         }));
       }
