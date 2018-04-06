b8a24a5b93c0e66f01c5494ab759e10b4d39cfc7
James Yen <jameshyen@gmail.com>
2018-04-06 09:21:06 -0700

    Test CSS

--- client/src/components/privacy/Privacy.jsx
+++ client/src/components/privacy/Privacy.jsx
@@ -3,66 +3,69 @@
 import React from 'react';
 
 const Privacy = () => (
-  <div>
-    <h3>Privacy Policy</h3>
-    Notorious NPM built the Rhyme Doctor app as a Free app. This SERVICE is provided by Notorious NPM at no cost and is intended for use as is.<br />
+  <div className="container-fluid">
+    <div className="row">
+      <h3>Privacy Policy</h3>
+      <p>
+        Notorious NPM built the Rhyme Doctor app as a Free app. This SERVICE is provided by Notorious NPM at no cost and is intended for use as is.
 
-    This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.<br />
+        This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
 
-    If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.<br />
+        If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
 
-    The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Rhyme Doctor unless otherwise defined in this Privacy Policy.<br />
+        The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Rhyme Doctor unless otherwise defined in this Privacy Policy.
+      </p>
+      <h4>Information Collection and Use</h4>
+      <p>
+        For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request is will be retained by us and used as described in this privacy policy.
 
-    <h4>Information Collection and Use</h4>
+        The app does use third party services that may collect information used to identify you.
 
-    For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request is will be retained by us and used as described in this privacy policy.<br />
+        Link to privacy policy of third party service providers used by the app
 
-    The app does use third party services that may collect information used to identify you.<br />
+        <a href="https://policies.google.com/privacy?hl=en&gl=us">Google Play Services</a>
+        Log Data
 
-    Link to privacy policy of third party service providers used by the app<br />
+        We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
+      </p>
+      <h4>Cookies</h4>
 
-    <a href="https://policies.google.com/privacy?hl=en&gl=us">Google Play Services</a><br />
-    Log Data<br />
+      Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory.<br />
 
-    We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.<br />
+      This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.<br />
 
-    <h4>Cookies</h4>
+      <h4>Service Providers</h4>
 
-    Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory.<br />
+      We may employ third-party companies and individuals due to the following reasons:<br />
 
-    This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.<br />
+      To facilitate our Service;<br />
+      To provide the Service on our behalf;<br />
+      To perform Service-related services; or<br />
+      To assist us in analyzing how our Service is used.<br />
+      We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.<br />
 
-    <h4>Service Providers</h4>
+      <h4>Security</h4>
 
-    We may employ third-party companies and individuals due to the following reasons:<br />
+      We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the Internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.<br />
 
-    To facilitate our Service;<br />
-    To provide the Service on our behalf;<br />
-    To perform Service-related services; or<br />
-    To assist us in analyzing how our Service is used.<br />
-    We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.<br />
+      <h4>Links to Other Sites</h4>
 
-    <h4>Security</h4>
+      This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.<br />
 
-    We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the Internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.<br />
+      <h4>Children’s Privacy</h4>
 
-    <h4>Links to Other Sites</h4>
+      These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.<br />
 
-    This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.<br />
+      <h4>Changes to This Privacy Policy</h4>
 
-    <h4>Children’s Privacy</h4>
+      We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.<br />
 
-    These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.<br />
+      <h4>Contact Us</h4>
 
-    <h4>Changes to This Privacy Policy</h4>
+      If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.<br />
 
-    We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.<br />
-
-    <h4>Contact Us</h4>
-
-    If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.<br />
-
-    This privacy policy page was created at <a href="https://privacypolicytemplate.net/">privacypolicytemplate.net</a> and modified/generated by <a href="https://app-privacy-policy-generator.firebaseapp.com/">App Privacy Policy Generator</a>
+      This privacy policy page was created at <a href="https://privacypolicytemplate.net/">privacypolicytemplate.net</a> and modified/generated by <a href="https://app-privacy-policy-generator.firebaseapp.com/">App Privacy Policy Generator</a>
+    </div>
   </div>
 );
 
