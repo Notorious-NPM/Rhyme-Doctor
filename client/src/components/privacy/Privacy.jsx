b500313a10cc2b558fd89ace3ee4132fa3b6fb70
James Yen <jameshyen@gmail.com>
2018-04-06 09:32:35 -0700

    Style

--- client/src/components/privacy/Privacy.jsx
+++ client/src/components/privacy/Privacy.jsx
@@ -2,6 +2,10 @@
 
 import React from 'react';
 
+const style = {
+  color: '#2a3439',
+};
+
 const Privacy = () => (
   <div className="container-fluid">
     <div className="row">
@@ -21,50 +25,52 @@ const Privacy = () => (
 
         The app does use third party services that may collect information used to identify you.
 
-        Link to privacy policy of third party service providers used by the app
-
-        <a href="https://policies.google.com/privacy?hl=en&gl=us">Google Play Services</a>
-        Log Data
-
+        Link to privacy policy of third party service providers used by the app:
+        <ul>
+          <li><a href="https://policies.google.com/privacy?hl=en&gl=us" style={style}>Google Play Services</a></li>
+          <li><a href="https://twitter.com/realDonaldTrump" style={style}>Log Data</a></li>
+        </ul>
         We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
       </p>
       <h4>Cookies</h4>
+      <p>
+        Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory.
 
-      Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your device internal memory.<br />
-
-      This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.<br />
-
+        This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collection information and to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
+      </p>
       <h4>Service Providers</h4>
-
-      We may employ third-party companies and individuals due to the following reasons:<br />
-
-      To facilitate our Service;<br />
-      To provide the Service on our behalf;<br />
-      To perform Service-related services; or<br />
-      To assist us in analyzing how our Service is used.<br />
-      We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.<br />
-
+      <p>
+      We may employ third-party companies and individuals due to the following reasons:
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li>To provide the Service on our behalf;</li>
+          <li>To perform Service-related services; or</li>
+          <li>To assist us in analyzing how our Service is used.</li>
+          <li>We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</li>
+        </ul>
+      </p>
       <h4>Security</h4>
-
-      We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the Internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.<br />
-
+      <p>
+        We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the Internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
+      </p>
       <h4>Links to Other Sites</h4>
-
-      This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.<br />
-
+      <p>
+        This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+      </p>
       <h4>Children’s Privacy</h4>
-
-      These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.<br />
-
+      <p>
+        These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
+      </p>
       <h4>Changes to This Privacy Policy</h4>
-
-      We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.<br />
-
+      <p>
+        We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.
+      </p>
       <h4>Contact Us</h4>
+      <p>
+        If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
 
-      If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.<br />
-
-      This privacy policy page was created at <a href="https://privacypolicytemplate.net/">privacypolicytemplate.net</a> and modified/generated by <a href="https://app-privacy-policy-generator.firebaseapp.com/">App Privacy Policy Generator</a>
+        This privacy policy page was created at <a href="https://privacypolicytemplate.net/" style={style}>privacypolicytemplate.net</a> and modified/generated by <a href="https://app-privacy-policy-generator.firebaseapp.com/" style={style}>App Privacy Policy Generator</a>
+      </p>
     </div>
   </div>
 );
